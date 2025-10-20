53cb5ab4c47b183851c0712f90afe75329504c39
Gustavoads12 <109917642+Gustavoads12@users.noreply.github.com>
2025-10-20 04:59:56 -0300

    Update landing-page.tsx

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
+import WistiaPlayer from "./WistiaPlayer";
 import Script from "next/script"
 
 export default function LandingPage() {
@@ -103,30 +104,7 @@ export default function LandingPage() {
               </h1>
             </div>
 
-          "use client";
-
-export default function WistiaPlayer() {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `
-          <script src="https://fast.wistia.com/player.js" async></script>
-          <script src="https://fast.wistia.com/embed/wz47cojpk6.js" async type="module"></script>
-          <style>
-            wistia-player[media-id='wz47cojpk6']:not(:defined) {
-              background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/wz47cojpk6/swatch');
-              display: block;
-              filter: blur(5px);
-              padding-top:177.78%;
-            }
-          </style>
-          <wistia-player media-id="wz47cojpk6" seo="false" aspect="0.5625"></wistia-player>
-        `
-      }}
-    />
-  );
-}
-
+         <WistiaPlayer />
 
         {/* Header */}
         <header className="bg-gradient-to-r from-purple-600 to-purple-800 text-white py-4 px-4 text-center">
