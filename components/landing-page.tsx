433809e14444fa326d336c9879a9379f07b2b140
Gustavoads12 <109917642+Gustavoads12@users.noreply.github.com>
2025-10-20 04:51:33 -0300

    Add WistiaPlayer component to landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -103,7 +103,30 @@ export default function LandingPage() {
               </h1>
             </div>
 
-           <script src="https://fast.wistia.com/player.js" async></script><script src="https://fast.wistia.com/embed/wz47cojpk6.js" async type="module"></script><style>wistia-player[media-id='wz47cojpk6']:not(:defined) { background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/wz47cojpk6/swatch'); display: block; filter: blur(5px); padding-top:177.78%; }</style> <wistia-player media-id="wz47cojpk6" seo="false" aspect="0.5625"></wistia-player>
+          "use client";
+
+export default function WistiaPlayer() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script src="https://fast.wistia.com/player.js" async></script>
+          <script src="https://fast.wistia.com/embed/wz47cojpk6.js" async type="module"></script>
+          <style>
+            wistia-player[media-id='wz47cojpk6']:not(:defined) {
+              background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/wz47cojpk6/swatch');
+              display: block;
+              filter: blur(5px);
+              padding-top:177.78%;
+            }
+          </style>
+          <wistia-player media-id="wz47cojpk6" seo="false" aspect="0.5625"></wistia-player>
+        `
+      }}
+    />
+  );
+}
+
 
         {/* Header */}
         <header className="bg-gradient-to-r from-purple-600 to-purple-800 text-white py-4 px-4 text-center">
